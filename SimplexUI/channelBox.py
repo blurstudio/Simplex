463491140bacc6bb2667146c4c61855c0d7e63c1
Tyler Fox <tbttfox@gmail.com>
2018-02-12 00:21:49 -0800

    Play with making the channelbox a tree.  Realize that all of these views should be implemented as proxy models of one master model that handles both sliders and combos

--- SimplexUI/channelBox.py
+++ SimplexUI/channelBox.py
@@ -13,7 +13,7 @@ from Qt.QtWidgets import (QTreeView, QListView, QApplication, QPushButton,
 from fnmatch import fnmatchcase
 from utils import getNextName, nested
 from contextlib import contextmanager
-from interfaceModel import Slider, Group, Simplex
+from interfaceModel import Slider, Group, Simplex, SimplexModel
 
 CONTEXT = os.path.basename(sys.executable)
 if CONTEXT == "maya.exe":
@@ -116,72 +116,6 @@ class SlideFilter(QObject):
 		self._slideStart = None
 		self.slideReleased.emit()
 
-
-class ChannelBoxModel(QAbstractItemModel):
-	def __init__(self, simplex, parent):
-		super(ChannelBoxModel, self).__init__(parent)
-		self.simplex = simplex
-		self.simplex.models.append(self)
-		self.channels = []
-
-	def setChannels(self, channels):
-		self.beginResetModel()
-		self.channels = channels
-		self.endResetModel()
-
-	def index(self, row, column=0, parIndex=QModelIndex()):
-		try:
-			item = self.channels[row]
-		except IndexError:
-			return QModelIndex()
-		return self.createIndex(row, column, item)
-
-	def parent(self, index):
-		return QModelIndex()
-
-	def rowCount(self, parent):
-		return len(self.channels)
-
-	def columnCount(self, parent):
-		return 1
-
-	def data(self, index, role):
-		if not index.isValid():
-			return None
-		item = index.internalPointer()
-
-		if role in (Qt.DisplayRole, Qt.EditRole):
-			if isinstance(item, (Group, Slider)):
-				return item.name
-
-		elif role == Qt.TextAlignmentRole:
-			return Qt.AlignCenter
-
-		return None
-
-	def flags(self, index):
-		return Qt.ItemIsEnabled | Qt.ItemIsEditable | Qt.ItemIsSelectable
-
-	def itemFromIndex(self, index):
-		return index.internalPointer()
-
-	def indexFromItem(self, item):
-		try:
-			row = self.channels.index(item)
-		except ValueError:
-			return QModelIndex()
-		return self.index(row)
-
-	def typeHandled(self, item):
-		if isinstance(item, Group):
-			return item.groupType == Slider
-		return isinstance(item, Slider)
-
-	def itemDataChanged(self, item):
-		if self.typeHandled(item):
-			idx = self.indexFromItem(item)
-			if idx.isValid():
-				self.dataChanged.emit(idx, idx)
 
 
 class ChannelBoxDelegate(QStyledItemDelegate):
@@ -301,9 +235,78 @@ class ChannelBoxDelegate(QStyledItemDelegate):
 			painter.restore()
 
 
-class ChannelBox(QListView):
+
+
+
+class ChannelListModel(QAbstractItemModel):
+	def __init__(self, simplex, parent):
+		super(ChannelListModel, self).__init__(parent)
+		self.simplex = simplex
+		self.simplex.models.append(self)
+		self.channels = []
+
+	def setChannels(self, channels):
+		self.beginResetModel()
+		self.channels = channels
+		self.endResetModel()
+
+	def index(self, row, column=0, parIndex=QModelIndex()):
+		try:
+			item = self.channels[row]
+		except IndexError:
+			return QModelIndex()
+		return self.createIndex(row, column, item)
+
+	def parent(self, index):
+		return QModelIndex()
+
+	def rowCount(self, parent):
+		return len(self.channels)
+
+	def columnCount(self, parent):
+		return 1
+
+	def data(self, index, role):
+		if not index.isValid():
+			return None
+		item = index.internalPointer()
+
+		if role in (Qt.DisplayRole, Qt.EditRole):
+			if isinstance(item, (Group, Slider)):
+				return item.name
+
+		elif role == Qt.TextAlignmentRole:
+			return Qt.AlignCenter
+
+		return None
+
+	def flags(self, index):
+		return Qt.ItemIsEnabled | Qt.ItemIsEditable | Qt.ItemIsSelectable
+
+	def itemFromIndex(self, index):
+		return index.internalPointer()
+
+	def indexFromItem(self, item):
+		try:
+			row = self.channels.index(item)
+		except ValueError:
+			return QModelIndex()
+		return self.index(row)
+
+	def typeHandled(self, item):
+		if isinstance(item, Group):
+			return item.groupType == Slider
+		return isinstance(item, Slider)
+
+	def itemDataChanged(self, item):
+		if self.typeHandled(item):
+			idx = self.indexFromItem(item)
+			if idx.isValid():
+				self.dataChanged.emit(idx, idx)
+
+class ChannelList(QListView):
 	def __init__(self, parent=None):
-		super(ChannelBox, self).__init__(parent)
+		super(ChannelList, self).__init__(parent)
 		self.slider = None
 		self._nxt = 0.0
 		self.start = False
@@ -354,8 +357,122 @@ class ChannelBox(QListView):
 
 
 
+
+
+
+class ChannelTreeModel(SimplexModel):
+	def getChildItem(self, parent, row):
+		try:
+			if isinstance(parent, Group):
+				return parent.items[row]
+			elif parent is None:
+				self.simplex.sliderGroups[row]
+		except IndexError:
+			pass
+		return None
+
+	def getItemRow(self, item):
+		row = None
+		if isinstance(item, Group):
+			row = item.simplex.sliderGroups.index(item)
+		elif isinstance(item, Slider):
+			row = item.group.items.index(item)
+		return row
+
+	def getParentItem(self, item):
+		par = None
+		if isinstance(item, Slider):
+			par = item.group
+		return par
+
+	def columnCount(self, parent):
+		return 1
+
+	def getItemRowCount(self, item):
+		if isinstance(item, Group):
+			return len(item.items)
+		elif item is None:
+			return len(self.simplex.sliderGroups)
+		return 0
+
+	def getItemAppendRow(self, item):
+		return self.getItemRowCount(item)
+
+	def getItemData(self, item, column, role):
+		if role in (Qt.DisplayRole, Qt.EditRole):
+			if column == 0:
+				if isinstance(item, Group):
+					return item.name
+				elif isinstance(item, Slider):
+					return item.name
+					#return "{0}: {1:.2f}".format(item.name, item.value)
+			print "BAD ICR", item, column, role
+			return "BAD"
+		return None
+
+	def typeHandled(self, item):
+		if isinstance(item, Group):
+			return item.groupType == Slider
+		return isinstance(item, Slider)
+
+class ChannelTree(QTreeView):
+	def __init__(self, parent=None):
+		super(ChannelTree, self).__init__(parent)
+		self.slider = None
+		self._nxt = 0.0
+		self.start = False
+		self.residual = 0.0
+
+	def slideStart(self):
+		p = self.mapFromGlobal(QCursor.pos())
+		item = self.indexAt(p).internalPointer()
+		if isinstance(item, Slider):
+			self.slider = item
+			self.start = True
+
+	def slideStop(self):
+		self.slider = None
+
+	def slideTick(self, val, offset, mul):
+		if self.slider is not None:
+			mx = self.slider.maxValue
+			mn = self.slider.minValue
+
+			tick = 20.0 / mul
+
+			if self.start or mul == 1.0:
+				self.start = False
+				val = (val * (mx - mn)) + mn
+				rn = round(val * tick) / tick
+			else:
+				# When working in relative mode, we keep track of the
+				# unused residual value and add it to the next tick.
+				# Because, unless each mouse move refresh is more than
+				# one full tick from the previous, we get no movement
+				val = (offset * (mx - mn))
+				val = self.slider.value + (val * mul)
+				val += self.residual
+				rn = round(val * tick) / tick
+				self.residual = val - rn
+
+			rn = min(max(rn, mn), mx)
+			# Do this to keep the ui snappy
+			self._nxt = rn
+			QTimer.singleShot(0, self.setval)
+
+	def setval(self):
+		if self.slider is not None and self._nxt is not None:
+			self.slider.value = self._nxt
+		self._nxt = None
+
+
+
+
+
+
+
 # DISPLAY TESTS
-def testSliderDisplay(smpxPath):
+def testSliderListDisplay(smpxPath):
 	simp = Simplex.buildSystemFromSmpx(smpxPath)
 	channels = []
 
@@ -388,8 +505,8 @@ def testSliderDisplay(smpxPath):
 	greyAttrs = set([u'lipsTogether'])
 
 	app = QApplication(sys.argv)
-	tv = ChannelBox()
-	model = ChannelBoxModel(simp, None)
+	tv = ChannelList()
+	model = ChannelListModel(simp, None)
 	delegate = ChannelBoxDelegate()
 
 	slideFilter = SlideFilter(tv.viewport())
@@ -420,11 +537,75 @@ def testSliderDisplay(smpxPath):
 	tv.show()
 	sys.exit(app.exec_())
 
+def testSliderTreeDisplay(smpxPath):
+	simp = Simplex.buildSystemFromSmpx(smpxPath)
+
+	redAttrs = set([
+		u'lowerLipDepressor_X', u'stretcher_X', u'platysmaFlex_X',
+		u'cheekRaiser_X', u'jawOpen', u'lidTightener_X',
+		u'outerBrowRaiser_X', u'eyesClosed_X', u'cornerPuller_X',
+		u'noseWrinkler_X', u'lipsBlow_X', u'cornerDepressor_X',
+		u'funneler', u'browLateral_X', u'innerBrowRaiser_X',
+		u'upperLipRaiser_X', u'chinRaiser', u'cheek_SuckBlow_X', u'pucker',
+		u'eyeGaze_DownUp_X', u'eyeGaze_RightLeft_X', u'upperLidTweak_X',
+		u'lowerLidTweak_X'
+	])
+	greenAttrs = set([
+		u'nasolabialDeepener_X', u'neckStretcher_X', u'lipsPressed_T',
+		u'lipsPressed_B', u'throatCompress', u'lipsRolled_InOut_B',
+		u'lipsRolled_InOut_T', u'sharpCornerPuller_X', u'dimpler_X',
+		u'eyeBlink_X', u'scalpSlide_BackFwd', u'browDown_X',
+		u'mouthSwing_RightLeft', u'sternoFlex_X', u'throatOpen'
+	])
+	blueAttrs = set([
+		u'adamsApple', u'noseSwing_RightLeft', u'nostrilCompress_X',
+		u'jawThrust_BackFwd', u'eyesWide_X', u'lipsVerticalT_X',
+		u'lipsVerticalB_X', u'earPull_X', u'lipsTighten_T',
+		u'lipsTighten_B', u'lipsCompress_T', u'lipsCompress_B',
+		u'lipsShift_RightLeft_B', u'lipsShift_RightLeft_T',
+		u'lipsNarrowT_X', u'lipsNarrowB_X', u'jawSwing_RightLeft',
+		u'nostril_SuckFlare_X', u'lipsCorner_DownUp_X', u'jawClench'
+	])
+	greyAttrs = set([u'lipsTogether'])
+
+	app = QApplication(sys.argv)
+	#tv = ChannelTree()
+	tv = QTreeView()
+	model = ChannelTreeModel(simp, None)
+	#delegate = ChannelBoxDelegate()
+
+	#slideFilter = SlideFilter(tv.viewport())
+	#slideFilter.slideButton = Qt.LeftButton
+	#tv.viewport().installEventFilter(slideFilter)
+	#slideFilter.slidePressed.connect(tv.slideStart)
+	#slideFilter.slideReleased.connect(tv.slideStop)
+	#slideFilter.slideTick.connect(tv.slideTick)
+
+	#for g in simp.sliderGroups:
+		#for item in g.items:
+			#if item.name in redAttrs:
+				#item.color = QColor(178, 103, 103)
+			#elif item.name in greenAttrs:
+				#item.color = QColor(90, 161, 27)
+			#elif item.name in blueAttrs:
+				#item.color = QColor(103, 141, 178)
+			#elif item.name in greyAttrs:
+				#item.color = QColor(130, 130, 130)
+
+	tv.setModel(model)
+	#tv.setItemDelegate(delegate)
+
+	tv.show()
+	sys.exit(app.exec_())
+
+
+
+
 
 if __name__ == "__main__":
 	basePath = r'D:\Users\tyler\Documents\GitHub\Simplex\scripts\SimplexUI\build'
 	#basePath = r'C:\Users\tfox\Documents\GitHub\Simplex\scripts\SimplexUI\build'
 	smpxPath = os.path.join(basePath, 'HeadMaleStandard_High_Unsplit.smpx')
 
-	testSliderDisplay(smpxPath)
+	testSliderTreeDisplay(smpxPath)
 
