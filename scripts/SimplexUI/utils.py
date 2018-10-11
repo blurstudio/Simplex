2f7f16af611bfb64fbf272c788dc29108536b238
Tyler Fox <tyler@blur.com>
2018-10-11 12:29:46 -0700

    Made sure that coerced and itemized lists are unique

--- scripts/SimplexUI/utils.py
+++ scripts/SimplexUI/utils.py
@@ -153,6 +153,10 @@ class singleShot(QObject):
 		self._args = []
 		self._function(inst, args)
 
+def makeUnique(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
 
 class nested(object):
 	"""Combine multiple context managers into a single nested context manager.
