f4fee7a79faee8ea88e35463b197e1568bfda05f
Tyler Fox <tyler@blur.com>
2018-10-16 19:45:49 -0700

    Make incremental Generation work

--- scripts/SimplexUI/tools/mayaPlugins/generateShapeIncrementals.py
+++ scripts/SimplexUI/tools/mayaPlugins/generateShapeIncrementals.py
@@ -1,13 +1,7 @@
 import maya.cmds as cmds
-from Qt.QtWidgets import QAction, QInputDialog
+from Qt.QtWidgets import QAction, QInputDialog, QMessageBox
 from SimplexUI.interfaceItems import Slider, Combo
 from SimplexUI.interfaceModel import coerceIndexToType
-
-def registerTool(window, menu):
-	generateShapeIncrementalsACT = QAction("Generate Incrementals", window)
-	menu.addAction(generateShapeIncrementalsACT)
-	kick = lambda: generateShapeIncrementalsInterface(window)
-	generateShapeIncrementalsACT.triggered.connect(kick)
 
 def registerContext(tree, indexes, menu):
 	window = tree.window()
@@ -29,45 +23,51 @@ def registerContext(tree, indexes, menu):
 	return True
 
 def generateShapeIncrementalsContext(indexes, window):
-	increments = QInputDialog.getInt(window, "Increments", "Number of Increments", 4, 1, 100)
-	if increments is None:
+	idx = indexes[0] # Only on the click index
+	slider = idx.model().itemFromIndex(idx)
+	if len(slider.prog.pairs) > 2:
+		QMessageBox.warning(window, "Warning", "Slider already has incrementals")
 		return
 
-	for idx in indexes:
-		slider = idx.model().itemFromIndex(idx)
-		rest = None
-		target = None
-		maxval = -1.0
-		for pp in slider.progression.pairs:
-			if pp.shape.isRest:
-				rest = pp.shape
-			elif abs(pp.value) > maxval:
-				target = pp.shape
-		restObj = slider.extractShape(rest, live=False)
-		tarObj = slider.extractShape(target)
-		generateShapeIncrementals(restObj, tarObj, increments)
+	increments, good = QInputDialog.getInt(window, "Increments", "Number of Increments", 4, 1, 100)
+	if not good:
+		return
 
-def generateShapeIncrementalsInterface(window):
-	sel = cmds.ls(sl=True)[0]
-	if len(sel) >= 2:
-		increments = QInputDialog.getInt(window, "Increments", "Number of Increments", 4, 1, 100)
-		if increments is None:
-			return
-		generateShapeIncrementals(sel[0], sel[1], increments)
+	rest = None
+	target = None
+	maxval = -1.0
 
-def generateShapeIncrementals(startObj, endObj, increments):
-	'''
-	Pick a start object, then an end object, and define a number of incremental steps
-	'''
-	shapeDup = cmds.duplicate(endObj, name="shapeDup")
+	for pp in slider.prog.pairs:
+		if pp.shape.isRest:
+			rest = pp.shape
+		elif abs(pp.value) > maxval:
+			target = pp.shape
+
+	target.name = target.name + "_100"
+
+	startObj = slider.extractShape(rest, live=False)
+	endObj = slider.extractShape(target)
+	shapeDup = cmds.duplicate(endObj, name="shapeDup")[0]
+
 	bs = cmds.blendShape(startObj, shapeDup)
 
+	incs = []
 	for i in range(1, increments):
 		val = float(increments - i) / increments
-		percent = int(float(i) * 100 /increments)
+		percent = int(float(i) * 100 / increments)
 		cmds.blendShape(bs, edit=True, weight=((0, val)))
-		cmds.duplicate(shapeDup, name="{0}_{1}".format(endObj, percent))
 
-	cmds.delete(shapeDup)
+		nne = endObj.replace("_100_", "_{0}_".format(percent))
+		nn = nne.replace("_Extract", "")
+		inc = cmds.duplicate(shapeDup, name=nne)
+		incs.append((percent, nn, nne))
+
+	for perc, inc, ince in incs:
+		pp = slider.createShape(shapeName=inc, tVal=perc/100.0)
+		pp.shape.connectShape(mesh=ince, live=True)
+
+	window.uiSliderTREE.model().invalidateFilter()
+
+	cmds.delete((shapeDup, startObj))
 	cmds.select(endObj)
 
