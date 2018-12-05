55d69e6b031b5dc170581c1c4e1245e941788857
Tyler Fox <tyler@blur.com>
2018-12-04 18:18:07 -0800

    OOps, forgot to add the generic ones

---
+++ SimplexUI/tools/genericPlugins/showFalloffs.py
@@ -0,0 +1,7 @@
+from Qt.QtWidgets import QAction
+
+def registerTool(window, menu):
+	editFalloffsACT = QAction("Edit Falloffs ...", window)
+	menu.addAction(editFalloffsACT)
+	editFalloffsACT.triggered.connect(window.showFalloffDialog)
+
