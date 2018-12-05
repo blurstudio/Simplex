76f1381945e418ff8c54e44e543897f32ecc582f
Tyler Fox <tyler@blur.com>
2018-12-04 18:17:01 -0800

    Move the generic tools into the generic folder

--- scripts/SimplexUI/tools/mayaPlugins/showFalloffs.py
+++
@@ -1,7 +0,0 @@
-from SimplexUI.Qt.QtWidgets import QAction
-
-def registerTool(window, menu):
-	editFalloffsACT = QAction("Edit Falloffs ...", window)
-	menu.addAction(editFalloffsACT)
-	editFalloffsACT.triggered.connect(window.showFalloffDialog)
-
