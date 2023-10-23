bb974a7969d8cbadc16f7925584329a42ce52b25
Tyler Fox <tyler@blur.com>
2023-10-23 16:41:46 -0700

    Switch to the limited api with a multi-phase init because I have an example

--- src/python/simplex_python.cpp
+++ src/python/simplex_python.cpp
@@ -18,13 +18,13 @@ PySimplex_dealloc(PySimplex* self) {
     Py_XDECREF(self->definition);
     if (self->sPointer != NULL)
 		delete self->sPointer;
-    Py_TYPE(self)->tp_free((PyObject*)self);
+    PyObject_Del(self);
 }
 
 static PyObject *
 PySimplex_new(PyTypeObject *type, PyObject *args, PyObject *kwds) {
-    PySimplex *self;
-    self = (PySimplex *)type->tp_alloc(type, 0);
+
+    PySimplex *self = PyObject_New(PySimplex, type);
     if (self != NULL) {
         self->definition = PyUnicode_FromString("");
         if (self->definition == NULL) {
@@ -59,14 +59,11 @@ PySimplex_setdefinition(PySimplex* self, PyObject* jsValue, void* closure){
     self->definition = jsValue;
     Py_DECREF(tmp);
 
-#if PY_MAJOR_VERSION >= 3
     // Get the unicode as a wstring, and a wstring to string converter
     std::wstring simDefw = std::wstring(PyUnicode_AsWideCharString(self->definition, NULL));
     std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
     std::string simDef = myconv.to_bytes(simDefw);
-#else
-    std::string simDef = std::string(PyString_AsString(self->definition));
-#endif
+
     // set the definition in the solver
     self->sPointer->clear();
     self->sPointer->parseJSON(simDef);
@@ -118,7 +115,7 @@ PySimplex_solve(PySimplex* self, PyObject* vec){
     PyObject *item;
     std::vector<double> stdVec, outVec;
     for (Py_ssize_t i=0; i<PySequence_Size(vec); ++i){
-        item = PySequence_ITEM(vec, i);
+        item = PySequence_GetItem(vec, i);
         if (! PyNumber_Check(item)) {
             PyErr_SetString(PyExc_TypeError, "Input list can contain only numbers");
             return NULL;
@@ -132,99 +129,10 @@ PySimplex_solve(PySimplex* self, PyObject* vec){
 
     PyObject *out = PyList_New(outVec.size());
     for (size_t i=0; i<outVec.size(); ++i){
-        PyList_SET_ITEM(out, i, PyFloat_FromDouble(outVec[i]));
+        PyList_SetItem(out, i, PyFloat_FromDouble(outVec[i]));
     }
     return out;
 }
-
-
-static PyObject *
-PySimplex_solveBuffer(PySimplex* self, PyObject* args){
-    PyObject *input, *output;
-	if (!PyArg_ParseTuple(args, "OO", &input, &output)) {
-		return NULL;
-	}
-
-    if (PyObject_CheckBuffer(input) == 0){
-        PyErr_SetString(PyExc_TypeError, "Input must be a buffer");
-        return NULL;
-    }
-    if (PyObject_CheckBuffer(output) == 0){
-        PyErr_SetString(PyExc_TypeError, "Output must be a buffer");
-        return NULL;
-    }
-
-    // Buffer WAY
-    Py_buffer inView, outView;
-
-    if (PyObject_GetBuffer(input, &inView, PyBUF_STRIDED_RO) != 0){
-        PyErr_SetString(PyExc_TypeError, "Cannot read input buffer");
-        return NULL;
-    }
-
-    if (PyObject_GetBuffer(output, &outView, PyBUF_STRIDED) != 0){
-        PyErr_SetString(PyExc_TypeError, "Cannot read output buffer");
-        PyBuffer_Release(&inView);
-        return NULL;
-    }
-
-    if (inView.ndim != 1){
-        PyErr_SetString(PyExc_ValueError, "Input must have exactly 1 dimension");
-        PyBuffer_Release(&inView);
-        PyBuffer_Release(&outView);
-        return NULL;
-    }
-
-    if (outView.ndim != 1){
-        PyErr_SetString(PyExc_ValueError, "Output must have exactly 1 dimension");
-        PyBuffer_Release(&inView);
-        PyBuffer_Release(&outView);
-        return NULL;
-    }
-
-    std::vector<double> stdVec, outVec;
-    stdVec.resize((size_t)inView.shape[0]);
-	Py_ssize_t inSize = inView.itemsize;
-		
-    char *iptr = (char *)inView.buf;
-    for (Py_ssize_t i = 0; i<inView.shape[0]; ++i){
-        if (inSize == sizeof(double)){
-            stdVec[i] = *(double *)iptr;
-        }
-        else {
-            stdVec[i] = (double)(*(float *)iptr);
-        }
-        iptr += inView.strides[0];
-    }
-
-	self->sPointer->clearValues();
-    outVec = self->sPointer->solve(stdVec);
-
-    if (outView.shape[0] < outVec.size()){
-        PyErr_SetString(PyExc_ValueError, "Output must have enough space allocated");
-        PyBuffer_Release(&inView);
-        PyBuffer_Release(&outView);
-        return NULL;
-    }
-
-	Py_ssize_t outSize = outView.itemsize;
-    if (outSize == sizeof(double)){
-		memcpy(outView.buf, outVec.data(), outVec.size() * sizeof(double));
-    }
-    else {
-        std::vector<float> outFloatVec;
-        outFloatVec.resize(outVec.size());
-        for (const auto &ov: outVec){
-            outFloatVec.push_back(ov);
-        }
-        memcpy(outView.buf, outVec.data(), outFloatVec.size()*sizeof(float));
-    }
-
-    PyBuffer_Release(&inView);
-    PyBuffer_Release(&outView);
-	Py_RETURN_NONE;
-}
-
 
 static PyGetSetDef PySimplex_getseters[] = {
     {(char*)"definition",
@@ -236,66 +144,205 @@ static PyGetSetDef PySimplex_getseters[] = {
      (getter)PySimplex_getexactsolve, (setter)PySimplex_setexactsolve,
      (char*)"Run the solve with the exact min() solver",
      NULL},
-    {NULL}  /* Sentinel */
+    {NULL}  // Sentinel
 };
 
 static PyMethodDef PySimplex_methods[] = {
     {(char*)"solve", (PyCFunction)PySimplex_solve, METH_O,
      (char*)"Supply an input list to the solver, and recieve and output list"
     },
-    {(char*)"solveBuffer", (PyCFunction)PySimplex_solveBuffer, METH_VARARGS,
-     (char*)"Supply an input list to the solver, and recieve and output buffer"
-    },
-    {NULL}  /* Sentinel */
+    {NULL}  // Sentinel
 };
 
+
+
+static PyType_Slot PySimplexType_Slots[] = {
+    {Py_tp_methods, PySimplex_methods},
+    {Py_tp_getset, PySimplex_getseters},
+    {Py_tp_init, (initproc)PySimplex_init},
+    {Py_tp_new, PySimplex_new},
+    {Py_tp_dealloc, (destructor)PySimplex_dealloc},
+    {0, 0},  // Sentinel
+};
+
+static PyType_Spec PySimplexType_Spec = {
+    .name = "pysimplex.PySimplex",
+    .basicsize = sizeof(PySimplex),
+    .flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .slots = PySimplexType_Slots,
+};
+
+
+
+static int PySimplexModule_exec(PyObject* module) {
+    PyObject* pysimplex_type;
+
+    pysimplex_type = PyType_FromSpec(&PySimplexType_Spec);
+    if (!pysimplex_type) {
+        Py_XDECREF(pysimplex_type);
+        Py_XDECREF(module);
+        return -1;
+    }
+
+    if (PyModule_AddObject(module, "PySimplex", pysimplex_type)) {
+        Py_XDECREF(pysimplex_type);
+        Py_XDECREF(module);
+        return -1;
+    }
+
+    return 0;
+}
+
+
+static PyModuleDef_Slot PySimplexModule_Slots[] = {
+    {Py_mod_exec, PySimplexModule_exec},
+    {0, NULL},
+};
+
+
+static PyModuleDef PySimplexModuleDef = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "pysimplex",
+    .m_doc = "The Simplex blendshape solver in Python",
+    .m_size = 0,
+    .m_methods = NULL,
+    .m_slots = PySimplexModule_Slots,
+};
+
+
+PyMODINIT_FUNC PyInit_pysimplex(void) {
+
+    return PyModuleDef_Init(&PySimplexModuleDef);
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+static PyTypeObject PySimplexType = {
+    .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "pysimplex.PySimplex",
+    .tp_doc = PyDoc_STR("PySimplex objects"),
+    .tp_methods = PySimplex_methods,
+    .tp_getset = PySimplex_getseters,
+    .tp_init = (initproc)PySimplex_init,
+    .tp_new = PySimplex_new,
+};
+
+static PyModuleDef PySimplexModuleDef = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "pysimplex",
+    .m_doc = "The Simplex blendshape solver in Python",
+    .m_size = -1,
+};
+
+
+static PyObject * localInit(void)
+{
+    PyObject* m;
+
+    // C++ doesn't like the tp flags out of order
+    // so I've gotta set some of 'em here
+    PySimplexType.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE;
+    PySimplexType.tp_basicsize = sizeof(PySimplex);
+    PySimplexType.tp_dealloc = (destructor)PySimplex_dealloc;
+
+    if (PyType_Ready(&PySimplexType) < 0)
+        return NULL;
+
+    m = PyModule_Create(&PySimplexModuleDef);
+
+    if (m == NULL)
+        return NULL;
+
+    Py_INCREF(&PySimplexType);
+    if (PyModule_AddObject(m, "PySimplex", (PyObject *) &PySimplexType) < 0) {
+        Py_DECREF(&PySimplexType);
+        Py_DECREF(m);
+        return NULL;
+    }
+    return m;
+
+}
+
+PyMODINIT_FUNC PyInit_pysimplex(void) {
+    return localInit();
+}
+
+*/
+
+
+
+
+
+
+
+/*
 static PyTypeObject PySimplexType = {
     PyVarObject_HEAD_INIT(NULL, 0)
-    "pysimplex.PySimplex",             /* tp_name */
-    sizeof(PySimplex),             /* tp_basicsize */
-    0,                         /* tp_itemsize */
-    (destructor)PySimplex_dealloc, /* tp_dealloc */
-    0,                         /* tp_print */
-    0,                         /* tp_getattr */
-    0,                         /* tp_setattr */
-    0,                         /* tp_compare */
-    0,                         /* tp_repr */
-    0,                         /* tp_as_number */
-    0,                         /* tp_as_sequence */
-    0,                         /* tp_as_mapping */
-    0,                         /* tp_hash */
-    0,                         /* tp_call */
-    0,                         /* tp_str */
-    0,                         /* tp_getattro */
-    0,                         /* tp_setattro */
-    0,                         /* tp_as_buffer */
+    "pysimplex.PySimplex",             // tp_name
+    sizeof(PySimplex),             // tp_basicsize
+    0,                         // tp_itemsize
+    (destructor)PySimplex_dealloc, // tp_dealloc
+    0,                         // tp_print
+    0,                         // tp_getattr
+    0,                         // tp_setattr
+    0,                         // tp_compare
+    0,                         // tp_repr
+    0,                         // tp_as_number
+    0,                         // tp_as_sequence
+    0,                         // tp_as_mapping
+    0,                         // tp_hash
+    0,                         // tp_call
+    0,                         // tp_str
+    0,                         // tp_getattro
+    0,                         // tp_setattro
+    0,                         // tp_as_buffer
     Py_TPFLAGS_DEFAULT |
-        Py_TPFLAGS_BASETYPE,   /* tp_flags */
-    "PySimplex objects",       /* tp_doc */
-    0,                         /* tp_traverse */
-    0,                         /* tp_clear */
-    0,                         /* tp_richcompare */
-    0,                         /* tp_weaklistoffset */
-    0,                         /* tp_iter */
-    0,                         /* tp_iternext */
-    PySimplex_methods,         /* tp_methods */
-    0,                         /* tp_members */
-    PySimplex_getseters,       /* tp_getset */
-    0,                         /* tp_base */
-    0,                         /* tp_dict */
-    0,                         /* tp_descr_get */
-    0,                         /* tp_descr_set */
-    0,                         /* tp_dictoffset */
-    (initproc)PySimplex_init,  /* tp_init */
-    0,                         /* tp_alloc */
-    PySimplex_new,             /* tp_new */
+        Py_TPFLAGS_BASETYPE,   // tp_flags
+    "PySimplex objects",       // tp_doc
+    0,                         // tp_traverse
+    0,                         // tp_clear
+    0,                         // tp_richcompare
+    0,                         // tp_weaklistoffset
+    0,                         // tp_iter
+    0,                         // tp_iternext
+    PySimplex_methods,         // tp_methods
+    0,                         // tp_members
+    PySimplex_getseters,       // tp_getset
+    0,                         // tp_base
+    0,                         // tp_dict
+    0,                         // tp_descr_get
+    0,                         // tp_descr_set
+    0,                         // tp_dictoffset
+    (initproc)PySimplex_init,  // tp_init
+    0,                         // tp_alloc
+    PySimplex_new,             // tp_new
 };
 
 static PyMethodDef module_methods[] = {
-    {NULL}  /* Sentinel */
+    {NULL}  // Sentinel
 };
 
-
+static struct PyModuleDef PySimplexModuleDef = {
+    PyModuleDef_HEAD_INIT,
+    "pysimplex",     // m_name
+    "The Simplex blendshape solver in Python",  // m_doc
+    -1,                  // m_size
+    module_methods,      // m_methods
+    NULL,                // m_reload
+    NULL,                // m_traverse
+    NULL,                // m_clear
+    NULL,                // m_free
+};
 
 static PyObject * localInit(void)
 {
@@ -303,24 +350,7 @@ static PyObject * localInit(void)
 
     if (PyType_Ready(&PySimplexType) < 0)
         return NULL;
-
-#if PY_MAJOR_VERSION >= 3
-    static struct PyModuleDef moduledef = {
-        PyModuleDef_HEAD_INIT,
-        "pysimplex",     /* m_name */
-        "The Simplex blendshape solver in Python",  /* m_doc */
-        -1,                  /* m_size */
-        module_methods,      /* m_methods */
-        NULL,                /* m_reload */
-        NULL,                /* m_traverse */
-        NULL,                /* m_clear */
-        NULL,                /* m_free */
-    };
-    m = PyModule_Create(&moduledef);
-#else
-    m = Py_InitModule3("pysimplex", module_methods,
-        "The Simplex blendshape solver in Python");
-#endif
+    m = PyModule_Create(&PySimplexModuleDef);
 
     if (m == NULL)
         return NULL;
@@ -330,12 +360,13 @@ static PyObject * localInit(void)
     return m;
 }
 
-#if PY_MAJOR_VERSION >= 3
 PyMODINIT_FUNC PyInit_pysimplex(void) {
     return localInit();
 }
-#else
-PyMODINIT_FUNC initpysimplex(void) {
-    localInit();
-}
-#endif
+*/
+
+
+
+
+
+
