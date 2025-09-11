e6670131229b90af4f12a36b3e7cdb5d20be4b0c
Tyler Fox <tyler@blur.com>
2025-09-10 17:27:07 -0700

    Separated building the python module vs building the python wheel.

--- quick_compile.bat
+++ quick_compile.bat
@@ -1,6 +1,6 @@
 setlocal
 
-SET MAYA_VERSION=2024
+SET MAYA_VERSION=2026
 REM "vs" "ninja"
 REM use VS for the debugger, otherwise use NINJA
 REM Until I figure out how to debug using nvim
@@ -12,12 +12,11 @@ SET BUILDDIR=mayabuild_%BUILDTYPE%_%MAYA_VERSION%_%BACKEND%
 if not exist %BUILDDIR%\ (
     meson setup %BUILDDIR% ^
     -Dmaya:maya_version=%MAYA_VERSION% ^
+    -Dmaya:maya_devkit_base=D:\Autodesk\MayaDev\Maya2026_2\devkitBase ^
     -Dmaya_build=true ^
-    -Dpython_build=false  ^
+    -Dpython_build=true  ^
     --buildtype %BUILDTYPE% --vsenv --backend %BACKEND%
 )
-
-
 
 if exist %BUILDDIR%\ (
     meson compile -C %BUILDDIR% -j 8
